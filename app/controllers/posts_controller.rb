b26ce58a711490876672ecc35d1dd1d9bdba1168
renzo <rensoliscastillo@gmail.com>
2019-11-29 12:04:10 -0500

    Finish implement of Posts logic

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,49 @@
 # frozen_literal_string: true
 
 class PostsController < ApplicationController
-	def index; end
+	before_action :set_post, only: %i[show edit update destroy]
 
-	def new; end
+	def index
+		@posts = Post.all.order('created_at DESC')
+	end
 
-	def create; end
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(post_params)
+		if @post.save
+			redirect_to @post
+		else
+			render :new
+		end
+	end
+
+	def show; end
+
+	def edit; end
+
+	def update
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@post.destroy
+		redirect_to posts_path
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :content)
+	end
+
+	def set_post
+		@post = Post.find params[:id]
+	end
 end
