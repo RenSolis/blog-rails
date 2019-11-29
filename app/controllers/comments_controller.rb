9943f53f454a062a47d4973fdc204aac908be2d0
renzo <rensoliscastillo@gmail.com>
2019-11-29 13:25:02 -0500

    add comments functionality

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,24 @@
+class CommentsController < ApplicationController
+	before_action :set_post
+
+	def create
+		@comment = @post.comments.create(comment_params)
+		redirect_to post_path(@post)
+	end
+
+	def destroy
+		@comment = @post.comments.find params[:id]
+		@comment.destroy
+		redirect_to post_path(@post)
+	end
+
+	private
+
+	def set_post
+		@post = Post.find params[:post_id]
+	end
+
+	def comment_params
+		params.require(:comment).permit(:name, :comment)
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-	resources :posts
+	resources :posts do
+		resources :comments, only: %i[create destroy]
+	end
 
 	root 'posts#index'
 end
